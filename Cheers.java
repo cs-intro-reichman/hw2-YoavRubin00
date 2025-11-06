9c8a1e976c36a1e184eb22c8f68c301c719c3aff
Yoav Rubin <yoav.rubin@post.runi.ac.il>
2025-11-06 17:57:34 +0200

    v04

--- Cheers.java
+++ Cheers.java
@@ -1,20 +1,20 @@
 public class Cheers {
     public static void main(String[] args) {
         String str = args[0];
-        String Num = args[1];
-        int n = Integer.parseInt(Num);
+        int n = Integer.parseInt(args[1]);
         String specialChars = "AEFHILMNORSX";
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            char chUpper = Character.toUpperCase(ch);
+            char chUpper = Character.toUpperCase(ch); 
             String article;
             if (specialChars.indexOf(chUpper) != -1) {
                 article = "an";
             } else {
-                article = "a";
+                article = "a "; 
             }
-            System.out.println("Give me " + article + " " + ch + ": " + ch + "!");
+            System.out.println("Give me " + article + " " + chUpper + ": " + chUpper + "!");
         }
+
         System.out.println("What does that spell?");
         String strUpper = str.toUpperCase();
         for (int i = 0; i < n; i++) {
